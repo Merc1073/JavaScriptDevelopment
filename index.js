e0562bdd80fe02e9f07bb4002765bc568aba6916
= <sergeikoblov2000@gmail.com>
2024-11-27 22:51:53 +0000

    Learned: Basics
    * console.log
    * window.alert
    * comments
    * getElementById

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log(`Hey there`)
+
+//window.alert(`This is an alert`)
+
+// This is a comment
+
+/*
+This is 
+a multi line 
+comment
+*/
+
+document.getElementById("helloH1").textContent = `Hello!`;
+document.getElementById("loremP1").textContent = `Now this isn't Lorem anymore`;
