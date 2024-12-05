be0150b80bbc9f722dfda03241cfa9cacb5ba1ac
= <sergeikoblov2000@gmail.com>
2024-12-05 02:42:44 +0000

    Practice Project: Dice Roll

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-console.log(Math.PI);
+const button = document.getElementById("rollButton");
+let randomNumber = document.getElementById("randomNumberLabel");
 
-console.log(Math.E);
+randomNumber.textContent = 5;
 
-let x = 6.37;
-let y = 8;
-let z;
-
-let a = -185;
-
-z = Math.round(x);
-
-console.log(Math.floor(x));
-console.log(Math.ceil(x));
-console.log(Math.trunc(x));
-console.log(Math.pow(x, y));
-console.log(Math.sqrt(x));
-
-console.log(Math.log(x));
-console.log(Math.sin(x));
-console.log(Math.cos(x));
-console.log(Math.tan(x));
-
-console.log(Math.abs(a));
-console.log(Math.sign(x));
-
-console.log(Math.max(x, y, z));
-console.log(Math.min(x, y, z));
+button.onclick = function()
+{
+    randomNumber.textContent = Math.floor(Math.random() * 6) + 1;
+}
