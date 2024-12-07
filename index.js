21343468410d3e264b0a352cb9c707f31f2ddfc3
= <sergeikoblov2000@gmail.com>
2024-12-07 17:45:59 +0000

    Learned: Functions

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
-const minNum = 1;
-const maxNum = 100;
+// Learning Functions
 
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-
-let attempts = 0;
-let guess;
-let running = true;
-
-while(running)
+function repetitiveCode()
 {
-    guess = window.prompt(`Guess the number between ${minNum} - ${maxNum}`);
-    guess = Number(guess);
+    console.log("This");
+    console.log("is");
+    console.log("a");
+    console.log("very");
+    console.log("long");
+    console.log("sentence!");
+}
 
-    if(isNaN(guess))
-    {
-        window.alert(`Please enter a valid number.`);
-    }
+repetitiveCode();
+repetitiveCode();
+repetitiveCode();
 
-    else if(guess > maxNum)
-    {
-        window.alert(`Number cannot exceed maximum number of ${maxNum}.`)
-    }
 
-    else if(guess < minNum)
+
+
+
+// Passing parameters/arguments
+function addTwoNums(a, b)
+{
+    return a + b;
+}
+
+let result = addTwoNums(4, 7);
+
+console.log(result);
+
+
+
+
+
+// Check if number is even or odd
+function evenOrOdd(num)
+{
+    if(num % 2 == 0)
     {
-        window.alert(`Number cannot go below minimum number of ${minNum}.`)
+        console.log(`${num} is even.`)
     }
 
     else
     {
-        attempts++;
-        if(guess < answer)
-        {
-            window.alert(`Too low!`)
-        }
-
-        else if(guess > answer)
-        {
-            window.alert(`Too high!`)
-        }
-
-        else
-        {
-            window.alert(`Correct! The answer is ${answer}. Total attempts: ${attempts}.`)
-            running = false;
-        }
+        console.log(`${num} is odd.`)
     }
-
 }
 
-console.log(answer);
+
+evenOrOdd(68);
