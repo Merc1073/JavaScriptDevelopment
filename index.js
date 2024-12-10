c84c8fa263a23b409f9194135b52294cb33e02d4
= <sergeikoblov2000@gmail.com>
2024-12-10 17:26:10 +0000

    Learned: Spread Operator

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-let arrayExample = ["one", "two", "three"];
+/*
 
-console.log(arrayExample[0]);
-console.log(arrayExample[1]);
-console.log(arrayExample[2]);
+    Spread operator: expands an iterable, such as a string or an array, into separate elements.
 
-arrayExample[2] = "four";
-console.log(arrayExample[2]);
-
-let numOfElements = arrayExample.length;
-
-let index = arrayExample.indexOf("two");
-console.log(index);
+*/
 
 
-for(let i = 0; i < numOfElements; i++)
-{
-    console.log(arrayExample[i]);
-}
 
-for(let num of arrayExample)
-{
-    console.log(num);
-}
+let numbers = [1, 2, 3, 4, 5];
 
-arrayExample.sort().reverse();
+//not spread (output: NaN)
+console.log(Math.max(numbers));
 
-for(let num of arrayExample)
-    {
-        console.log(num);
-    }
+//spread (output: 5)
+console.log(Math.max(...numbers));
+
+
+
+//combining arrays
+let fruits = ["apple", "orange", "banana"];
+let vegetables = ["cucumber", "lettuce", "radish"];
+
+let food = [...fruits, ...vegetables];
+
+console.log(...fruits);
+console.log(...vegetables);
+
+console.log(...food);
